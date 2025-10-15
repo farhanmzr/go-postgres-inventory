572aeb8805e2f9f857dd2ea7b6853d4f52a9d77a
farhanmazario <farhan.mazario@code.id>
2025-10-15 15:16:36 +0300

    fix model barang

--- models/barang.go
+++ models/barang.go
@@ -6,14 +6,14 @@ type Barang struct {
 	gorm.Model
 	Nama         string     `json:"nama"`
 	Kode         string     `json:"kode" gorm:"unique"`
-	GudangID     uint       `json:"gudang_id"` // foreign key ke Gudang
-	Gudang       Gudang     `json:"gudang"`    // preload
+	GudangID     uint       `json:"gudang_id"`                         // foreign key ke Gudang
+	Gudang       Gudang     `gorm:"foreignKey:GudangID" json:"gudang"` // preload
 	LokasiSusun  string     `json:"lokasi_susun"`
 	Satuan       string     `json:"satuan"`
 	Merek        string     `json:"merek"`
 	MadeIn       string     `json:"made_in"`
-	GrupBarangID uint       `json:"grup_barang_id"` // foreign key ke KodeGrupBarang
-	GrupBarang   GrupBarang `json:"grup_barang"`    // preload
+	GrupBarangID uint       `json:"grup_barang_id"`                             // foreign key ke GrupBarang
+	GrupBarang   GrupBarang `gorm:"foreignKey:GrupBarangID" json:"grup_barang"` // preload
 	HargaBeli    float64    `json:"harga_beli"`
 	HargaJual    float64    `json:"harga_jual"`
 	Stok         int        `json:"stok"`
