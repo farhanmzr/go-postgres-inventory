9bdb38095f7b6df6e75ada481b38280077106db7
farhanmazario <farhan.mazario@code.id>
2025-10-19 11:11:08 +0300

    fix bug

--- models/admin.go
+++ models/admin.go
@@ -4,14 +4,14 @@ import "time"
 
 type Admin struct {
 	ID           uint       `gorm:"primaryKey"                       json:"id"`
-	Username     string     `gorm:"uniqueIndex;size:120;not null"    json:"username"`
-	FullName     string     `gorm:"size:180;not null"                json:"full_name"`
+	Username     string     `gorm:"uniqueIndex;size:120"    json:"username"`
+	FullName     string     `gorm:"size:180"                json:"full_name"`
 	AdminCode    string     `gorm:"size:60"                          json:"admin_code"`
 	Position     string     `gorm:"size:120"                         json:"position"`
 	Phone        string     `gorm:"size:60"                          json:"phone"`
 	Address      string     `gorm:"size:255"                         json:"address"`
 	AvatarURL    string     `gorm:"size:255"                         json:"avatar_url"`
-	PasswordHash string     `gorm:"size:255;not null"                json:"-"` // disembunyikan di JSON
+	PasswordHash string     `gorm:"size:255"                json:"-"` // disembunyikan di JSON
 	IsActive     bool       `gorm:"default:true"                     json:"is_active"`
 	LastLoginAt  *time.Time `json:"last_login_at"`
 	CreatedAt    time.Time  `json:"created_at"`
--- models/user.go
+++ models/user.go
@@ -4,15 +4,15 @@ import "time"
 
 type User struct {
 	ID           uint       `gorm:"primaryKey"                       json:"id"`
-	Username     string     `gorm:"uniqueIndex;size:120;not null"    json:"username"`
-	FullName     string     `gorm:"size:180;not null"                json:"full_name"`
+	Username     string     `gorm:"uniqueIndex;size:120"    json:"username"`
+	FullName     string     `gorm:"size:180"                json:"full_name"`
 	UserCode     string     `gorm:"size:60"                          json:"user_code"`
 	Position     string     `gorm:"size:120"                         json:"position"`
 	WorkLocation string     `gorm:"size:120"                         json:"work_location"`
 	Phone        string     `gorm:"size:60"                          json:"phone"`
 	Address      string     `gorm:"size:255"                         json:"address"`
 	AvatarURL    string     `gorm:"size:255"                         json:"avatar_url"`
-	PasswordHash string     `gorm:"size:255;not null"                json:"-"` // jangan dikirim ke client
+	PasswordHash string     `gorm:"size:255"                json:"-"` // jangan dikirim ke client
 	IsActive     bool       `gorm:"default:true"                     json:"is_active"`
 	LastLoginAt  *time.Time `json:"last_login_at"`
 	CreatedAt    time.Time  `json:"created_at"`
