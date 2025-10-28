abb8553d925c688b683a7af8fd12a71c3c320a98
farhanmazario <farhan.mazario@code.id>
2025-10-28 10:29:22 +0200

    fix model salesreqid

--- models/penjualan.go
+++ models/penjualan.go
@@ -12,16 +12,16 @@ const (
 )
 
 type SalesRequest struct {
-	ID           uint          `gorm:"primaryKey" json:"id"`
-	TransCode    string        `gorm:"uniqueIndex;size:40" json:"trans_code"` // e.g. TR-2025-000123 (generate di server)
-	ManualCode   *string       `gorm:"size:40" json:"manual_code"`            // opsional, admin isi
-	Username     string        `gorm:"size:180;not null" json:"username"`
-	SalesDate time.Time     `json:"sales_date"` // tanggal (<= today)
-	WarehouseID  uint          `json:"warehouse_id"`
-	Warehouse    Gudang        `json:"warehouse"`
-	CustomerID   uint          `json:"customer_id"`
-	Customer     Customer      `json:"customer"`
-	Payment      PaymentMethod `gorm:"size:10" json:"payment"`
+	ID          uint          `gorm:"primaryKey" json:"id"`
+	TransCode   string        `gorm:"uniqueIndex;size:40" json:"trans_code"` // e.g. TR-2025-000123 (generate di server)
+	ManualCode  *string       `gorm:"size:40" json:"manual_code"`            // opsional, admin isi
+	Username    string        `gorm:"size:180;not null" json:"username"`
+	SalesDate   time.Time     `json:"sales_date"` // tanggal (<= today)
+	WarehouseID uint          `json:"warehouse_id"`
+	Warehouse   Gudang        `json:"warehouse"`
+	CustomerID  uint          `json:"customer_id"`
+	Customer    Customer      `json:"customer"`
+	Payment     PaymentMethod `gorm:"size:10" json:"payment"`
 
 	Status       SalesStatus `gorm:"size:12;index" json:"status"`
 	RejectReason *string     `gorm:"size:255" json:"reject_reason"`
@@ -34,11 +34,11 @@ type SalesRequest struct {
 }
 
 type SalesReqItem struct {
-	ID                uint   `gorm:"primaryKey" json:"id"`
-	PurchaseRequestID uint   `gorm:"index" json:"purchase_request_id"`
-	BarangID          uint   `json:"barang_id"`
-	Barang            Barang `json:"barang"`
-	Qty               int64  `json:"qty"`
-	SellPrice         int64  `json:"sell_price"` // harga jual saat request
-	LineTotal         int64  `json:"line_total"`
+	ID             uint   `gorm:"primaryKey" json:"id"`
+	SalesRequestID uint   `gorm:"index" json:"sales_request_id"`
+	BarangID       uint   `json:"barang_id"`
+	Barang         Barang `json:"barang"`
+	Qty            int64  `json:"qty"`
+	SellPrice      int64  `json:"sell_price"` // harga jual saat request
+	LineTotal      int64  `json:"line_total"`
 }
